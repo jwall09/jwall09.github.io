aea251d06b927f3edf8f5beb54c9dbab24f0906a
jwall09 <97980119+jwall09@users.noreply.github.com>
2022-05-25 17:12:27 -0500

    time to button up

--- js/projects.js
+++ js/projects.js
@@ -4,36 +4,35 @@ Coding 07-09
 Purpose: JavaScript for use on projects page
 **********************/
 
-let imageItems = [...document.querySelectorAll('.img-wrap')];
-let titles = [...document.querySelectorAll('.h2')];
+// let imageItems = [...document.querySelectorAll('.img-wrap')];
+// let titles = [...document.querySelectorAll('.h2')]
+// let sections = [];
 
-let options = {
-    rootMargin: '0px',
+// let options = {
+//     rootMargin: '0px',
+//     threshold: .2
+//   }
 
-    //targets top 20% of object theoretically coming into view before it is triggered
-    threshold: .2
-}
 
-let setItemActive = (entries => {
-    console.log(entries)
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('active')
-        }
-    })
-});
+// let setItemActive = (entries, observer) => {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('active');
+//         }
+       
+//     })
+// }
 
-let observer = new IntersectionObserver(setItemActive, options);
+// let observer = new IntersectionObserver(setItemActive, options);
+  
+// imageItems.forEach((item,idx) => {
+//     item.children[0].style.backgroundImage = `url(../images/${idx+1}.png)`
+//     idx % 2 == 0 ? item.style.left = '55%' : item.style.left = '5%'
+//     observer.observe(item)
+// })
 
-imageItems.forEach((item, idx) => {
-    item.children[0].style.backgroundImage = `url(./images/${idx+1}.png)`;
-    idx % 2 == 0 ? item.style.left = '55%' : item.style.left = '5%';
-    observer.observe(item)
-})
-
-titles.forEach((title, idx) =>{
-    idx % 2 == 0 ? title.style.left = '45%' : title.style.left = '35%';
-    observer.observe(title)
-} )
-
+// titles.forEach((title, idx) => {
+//     idx % 2 == 0 ? title.style.left = '45%' : title.style.left = '35%';
+//     observer.observe(title)
+// })
 
